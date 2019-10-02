ee328a12324c420a911fb53afa95035a3cd427c4
arc0801 <54796547+arc0801@users.noreply.github.com>
2019-10-02 16:14:35 +0900

    TeamPlay_B

--- src/com/arc/control/BonusController.java
+++ src/com/arc/control/BonusController.java
@@ -29,10 +29,10 @@ public class BonusController {
 
 		while(check) {
 			System.out.println("++++++++++++++++++++");
-			System.out.println("1. 급여등급 전체 정보 출력");
-			System.out.println("2. 급여등급 검색 출력");
-			System.out.println("3. 급여등급 정보 추가");
-			System.out.println("4. 급여등급 정보 삭제");
+			System.out.println("1. 전체 정보 출력");
+			System.out.println("2. 검색 출력");
+			System.out.println("3. 정보 추가");
+			System.out.println("4. 정보 삭제");
 			System.out.println("5. 종료");
 			System.out.println("++++++++++++++++++++");
 			select = sc.nextInt();
