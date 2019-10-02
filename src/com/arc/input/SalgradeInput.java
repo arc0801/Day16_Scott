d2d60f7bff49e3c3b471b73f8bbe46f26459ee43
arc0801 <54796547+arc0801@users.noreply.github.com>
2019-10-02 14:51:13 +0900

    salgrade

--- src/com/arc/input/SalgradeInput.java
+++ src/com/arc/input/SalgradeInput.java
@@ -1,5 +1,37 @@
 package com.arc.input;
 
+import java.util.Scanner;
+
+import com.arc.salgrade.SalgradeDTO;
+
 public class SalgradeInput {
 
+	private Scanner sc;
+	
+	public SalgradeInput() {
+		sc = new Scanner(System.in);
+	}
+	
+	public SalgradeDTO insert() {
+		SalgradeDTO salgradeDTO = new SalgradeDTO();
+		
+		System.out.println("추가할 급여등급을 입력하세요.");
+		salgradeDTO.setGrade(sc.nextInt());
+		System.out.println("추가할 최저급여를 입력하세요.");
+		salgradeDTO.setLosal(sc.nextInt());
+		System.out.println("추가할 최고급여를 입력하세요.");
+		salgradeDTO.setHisal(sc.nextInt());
+		
+		return salgradeDTO;
+	}
+	
+	
+	public int salgradeInput() {
+		
+		System.out.println("급여등급을 입력하세요");
+		int grade = sc.nextInt();
+		
+		return grade;
+	}
+	
 }
